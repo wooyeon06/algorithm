537530b532ae8b10b28e1355cf2313e46e8a9a59
wooyeon06 <wooyeon06@gmail.com>
2024-12-24 10:06:50 +0900

    t

--- algorithm/src/temp/TestMain.java
+++ algorithm/src/temp/TestMain.java
@@ -11,9 +11,7 @@ public class TestMain {
 		// TODO Auto-generated method stub
 		Solution s = new Solution();
 		Object obj = s.solution(
-			new int[] {2, 4, 5, 3, 2, 1, 4, 2, 5, 1}, 3 //3
-			//new int[] {5,5,2,3,4,5,6,2,2,3,4,5,6}, 2 //3
-			//new int[] {7,2,8,7,2,5,9}, 3 //7
+				new int[][] {{0, 0, 0, 1, 1},{0, 0, 0, 1, 0},{0, 1, 0, 1, 1},{1, 1, 0, 0, 1},{0, 0, 0, 0, 0}}
 		);
 
 		System.out.println(obj);
